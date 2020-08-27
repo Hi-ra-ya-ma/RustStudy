86d8690bffb29b895ab71cce3bb2b92680d96b34
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-08-28 08:20:56 +0900

    Update: 基本の型定義と関数の定義

--- basic-type-definition/src/main.rs
+++ basic-type-definition/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // 型推論
+    let name = "hirayama";
+    let age1 = 20;
+
+    // 型定義
+    let fullName: &str = "hirayama";
+    let age2: i32 = 21;
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
