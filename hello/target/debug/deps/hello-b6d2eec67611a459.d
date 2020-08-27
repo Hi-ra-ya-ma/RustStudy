0d9e6729b9d95ef06e95c5b6f76fe29c958a3fea
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-08-27 14:16:09 +0900

    Add: 作成したプロジェクトをビルド
    cargo build

---
+++ hello/target/debug/deps/hello-b6d2eec67611a459.d
@@ -0,0 +1,5 @@
+/workspaces/RustStudy/hello/target/debug/deps/hello-b6d2eec67611a459: src/main.rs
+
+/workspaces/RustStudy/hello/target/debug/deps/hello-b6d2eec67611a459.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/workspaces/RustStudy/hello/target/debug/hello: /workspaces/RustStudy/hello/src/main.rs
