c7bc4f9ca6851b3a6d2de3d30f73f379246a0223
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-09-01 08:07:57 +0900

    Update: 借用の解決（参照型）
    「＆」を付与することで変数が参照型（Reference）となる。
    参照型を利用することで、変数の中身を借用せずに処理を行うことができる。

--- basic-type-definition/src/main.rs
+++ basic-type-definition/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let x = String::from("Hello");
-    let len = string_length(x);
+    let len = string_length(&x);
     println!("len is {}", len);
-    println!("x is {}", x); // コンパイルエラー！
+    println!("x is {}", x);
 }
 
-fn string_length(s: String) -> usize {
+fn string_length(s: &String) -> usize {
     let length = s.len();
     length
 }
