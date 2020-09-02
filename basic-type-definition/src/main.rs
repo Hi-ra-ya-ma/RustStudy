a46789e45fb17ed4279983b5ba4f768390cacccb
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-09-02 09:34:14 +0900

    Update: 束縛について
    Rustではletで定義した変数はあとから変更ができない変数となる。→ これを束縛という。
    また定数は別途存在していて、束縛された変数（不変変数？）との違いは以下の通り。
    ・mutキーワードが使用できない → 定数は常に不変である
    ・定数はどんなスコープでも定義することができる
    ・定数は定数式にしか利用できない → 式や関数の戻り値には使用できない

--- basic-type-definition/src/main.rs
+++ basic-type-definition/src/main.rs
@@ -9,3 +9,11 @@ fn string_length(s: &String) -> usize {
     let length = s.len();
     length
 }
+
+fn main2() {
+    let x = 100;
+    println!("x is {}", x);
+
+    x = 200; // Error!
+    println!("x is {}", x);
+}
